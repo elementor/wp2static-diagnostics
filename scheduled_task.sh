5a7bd4e6d02f3f3e30e6ed551a74a5a62ff30998
Leon Stafford <lionhive@gmail.com>
2018-12-18 19:26:06 +0700

    allow override wp cmd

--- scheduled_task.sh
+++ scheduled_task.sh
@@ -7,42 +7,44 @@ cd $WP2STATICSCRIPTSDIR
 
 cd $WPDIR
 
+WPCLI = "$(which wp) $WPCMDAPPEND"
+
 # remove previous version, while preserving settings.
-wp plugin deactivate --uninstall wordpress-static-html-plugin
+$WPCLI plugin deactivate --uninstall wordpress-static-html-plugin
 
 # install latest development version
-wp plugin install https://github.com/leonstafford/wp2static/archive/master.zip
+$WPCLI plugin install https://github.com/leonstafford/wp2static/archive/master.zip
 
 # rename folder for correct plugin slug
 mv wp-content/plugins/wp2static wp-content/plugins/wordpress-static-html-plugin
 
 # activate the renamed plugin
-wp plugin activate wordpress-static-html-plugin
+$WPCLI plugin activate wordpress-static-html-plugin
 
 # set options for Netlify deploy
-wp wp2static option netlifySiteID $NETLIFYSITEID
-wp wp2static option netlifyPersonalAccessToken $NETLIFYACCESSTOKEN
-wp wp2static option baseUrl https://$NETLIFYSITEID
-wp wp2static option baseUrl-netlify https://$NETLIFYSITEID
-wp wp2static option useBasicAuth $USEBASICAUTH
-wp wp2static option basicAuthUser $BASICAUTHUSER
-wp wp2static option basicAuthPassword $BASICAUTHPASS
+$WPCLI wp2static option netlifySiteID $NETLIFYSITEID
+$WPCLI wp2static option netlifyPersonalAccessToken $NETLIFYACCESSTOKEN
+$WPCLI wp2static option baseUrl https://$NETLIFYSITEID
+$WPCLI wp2static option baseUrl-netlify https://$NETLIFYSITEID
+$WPCLI wp2static option useBasicAuth $USEBASICAUTH
+$WPCLI wp2static option basicAuthUser $BASICAUTHUSER
+$WPCLI wp2static option basicAuthPassword $BASICAUTHPASS
 
 # quick test
-wp wp2static diagnostics
+$WPCLI wp2static diagnostics
 
 # install theme for running diagnostics
-wp theme install https://github.com/leonstafford/diagnostic-theme-for-wp2static/archive/master.zip --activate
+$WPCLI theme install https://github.com/leonstafford/diagnostic-theme-for-wp2static/archive/master.zip --activate
 
 # generate an archive
-wp wp2static generate
+$WPCLI wp2static generate
 
 # pipe generate time into a TXT file and have this loaded by the theme via JS...
 
 # this allows for some general benchmarking/comparison across hosts
 
 # test deploy
-wp wp2static deploy --test
+$WPCLI wp2static deploy --test
 
 # deploy (to folder "/mystaticsite/" if no existing options set)
-wp wp2static deploy
+$WPCLI wp2static deploy
